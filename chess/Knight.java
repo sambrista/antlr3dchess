216f7f23cf5fc5335d027f3dca3bf547ad36205e
aljimvil <aljimvil@c2c813c6-3edb-11de-917f-4137a9cc17b9>
2009-05-12 10:29:00 +0000

    Añadidas funciones para que cada pieza devuelva el fichero que la contiene

--- chess/Knight.java
+++ chess/Knight.java
@@ -8,6 +8,9 @@ public class Knight extends Piece {
 	public String getKindString() {
 		return "Knight";
 	}
+	public String get3Dfile() {
+		return "Caballo" + (this.getColor() == Piece.Color.WHITE ? "B" : "N") + ".wrl";
+	}
 	public boolean canMoveTo(int target_row, int target_column) {
 		if ( Math.sqrt(Math.pow(target_column - this.getColumn(), 2) + Math.pow(target_row - this.getRow(), 2)) == Math.sqrt(5)) {
 			return true;
